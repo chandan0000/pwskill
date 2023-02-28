3dad7d2235fed04dd45933db905823e8773c751c
chandan kumar <kumarchandan41u@gmail.com>
2023-02-28 10:51:31 +0530

    data scince course by pwskill

---
+++ pwskill/payment/payment_details.py
@@ -0,0 +1,10 @@
+import os ,sys
+from os.path import dirname , join , abspath
+
+sys.path.insert(0, abspath(join(dirname(__file__),'')))
+
+from course import course_details
+def payment1():
+    print("this is payment deatils")
+course_details.course()
+tdyhtryhtruytryeradydryertytrstfydreytrg
